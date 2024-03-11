8cb93f3a5fdd59a79e1192880397b86a33c62009
AMSARASWATHI <202100586@sigc.edu>
2024-03-11 04:05:12 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://images.shiksha.com/mediadata/images/articles/1699533840phpE7YQ3V.jpeg" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://react-blog-app-backend-olive.vercel.app"
